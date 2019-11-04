6dee8254538b15a0d031e80bf939b92af24b5b30
zjy5755202 <1181370590@qq.com>
2019-11-05 00:17:07 +0800

    note: xxx

--- src/main/java/com/whu/libingteam/config/redisConfig.java
+++ src/main/java/com/whu/libingteam/config/redisConfig.java
@@ -40,6 +40,8 @@ public class redisConfig {
         //使用StringRedisSerializer来序列化和反序列化redis的key值
         template.setKeySerializer(new StringRedisSerializer());
 
+        System.out.println();
+
         // 设置hash key 和value序列化模式
         template.setHashKeySerializer(new StringRedisSerializer());
         template.setHashValueSerializer(jacksonSeial);
